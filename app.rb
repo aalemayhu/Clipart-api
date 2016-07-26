64ef8d77ffd7a8addfecee1fe4191df313223f6f
Alexander Alemayhu <alexander@alemayhu.com>
2016-07-26 14:51:02 +0000

    requests to openclipart.org are now cached

--- app.rb
+++ app.rb
@@ -1,10 +1,22 @@
 require 'sinatra'
+require 'httparty'
+require 'json'
 
+tmpDirectory = "/tmp/requests-clipart"
 get '/' do
   version = `git describe --tags` 
   "#{version}"
 end
 
 get '/search' do
-  "search"
+  content_type :json
+  query = params['query'] || "play"
+  amount = params['amount'] || "20"
+  `mkdir -p #{tmpDirectory}`
+  queryUrl = "https://openclipart.org/search/json/?query=#{query}&amount=#{amount}"
+  response = HTTParty.get(queryUrl)
+  payload = response.parsed_response.to_json
+  File.open("#{tmpDirectory}/#{query}.#{amount}.txt", 'w') { |file| file.write(payload) }
+
+  payload
 end
