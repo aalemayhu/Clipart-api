e2d9389f8c47f2540e9edf7f62f77feb845ce89a
Alexander Alemayhu <alexander@alemayhu.com>
2016-07-26 14:58:01 +0000

    Refactor and comment

--- app.rb
+++ app.rb
@@ -2,20 +2,32 @@ require 'sinatra'
 require 'httparty'
 require 'json'
 
+# For testing purposes let's persist all responses
 tmpDirectory = "/tmp/requests-clipart"
+`mkdir -p #{tmpDirectory}`
+
+# Make it easy to know what's running on the server
 get '/' do
   version = `git describe --tags` 
   "#{version}"
 end
 
+# Use the parameters to build the payload resource
+def queryUrl(query, amount)
+  "https://openclipart.org/search/json/?query=#{query}&amount=#{amount}"
+end
+
 get '/search' do
+  # The caller is expecting a json paylod
   content_type :json
+
   query = params['query'] || "play"
   amount = params['amount'] || "20"
-  `mkdir -p #{tmpDirectory}`
-  queryUrl = "https://openclipart.org/search/json/?query=#{query}&amount=#{amount}"
-  response = HTTParty.get(queryUrl)
+
+  response = HTTParty.get(queryUrl(query, amount))
   payload = response.parsed_response.to_json
+
+  # Write the response to disk for further testing
   File.open("#{tmpDirectory}/#{query}.#{amount}.txt", 'w') { |file| file.write(payload) }
 
   payload
